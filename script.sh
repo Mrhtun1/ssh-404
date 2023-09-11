64c56895254b069ab374584221fa3e86917fd190
Mrhtun1 <144703038+Mrhtun1@users.noreply.github.com>
2023-09-11 23:12:30 +0630

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,24 +17,24 @@ none='\e[0m'
 
 sed -i 's/#\?AllowTcpForwarding .*/AllowTcpForwarding yes/' /etc/ssh/sshd_config && sed -i 's/#\?PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && sed -i 's/#\?Banner .*/Banner \/etc\/ssh\/gcp_404/' /etc/ssh/sshd_config && /etc/init.d/ssh restart;
 echo "
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='red'>----------©©®®Gcp Sever©©®®----------
 </font></h3>
-<h3><font color='magenta'>--- ۩ PREMIUM SERVER ۩ ---
-</font></h3>
-
-<h3><font color='green'>--- ۩ SERVER BY ④ ⍤⃝🌻④ ۩ ---
+<h3><font color='green'>🚀🚀🚀MR.HTUN GCP SEVER🚀🚀🚀
 </font></h3>
 
-<h4><font color='cyan'>Telegram Channel >> https://t.me/Pmttg
+<h3><font color='green'>🎭🗄️🎭Premium SEVER🎭 🗄️🎭
+</font></h3>
+
+<h4><font color='cyan'>Telegram Channel >>အခြားသူရဲ့ပျော်ရွှင်မှုဟာ သင့်ရဲ့ပျော်ရွှင်မှုထက် ပိုပြီး အရေးကြီးတယ်ဆိုတာဟာ ချစ်ခြင်းမေတ္တာ ဖြစ်ပေါ်ချိန်ပါပဲ။#crd
 </font></h4>
 
-<h4><font color='blue'>Telegram Channel >> https://t.me/nkka_404
+<h4><font color='blue'>Telegram Channel >>My Channel https://t.me/freegcp2
 </font></h4>
 
-<h3><font color='red'>▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬
+<h3><font color='red'>----------©©®®Gcp Sever©©®®----------
 </font></h3>" | tee /etc/ssh/gcp_404 >/dev/null
-useradd "404" --shell=/bin/false -M
-echo "404:404" | chpasswd
+useradd "MrHtun" --shell=/bin/false -M
+echo "MrHtun:MrHtun" | chpasswd
 
 echo ""
 
@@ -49,4 +49,4 @@ echo -e "${cyan}          \|__|        \|_______|              \|__|            
 echo -e "${green}Contact the developer https://t.me/nkka404 for more information              ${plain}"
 echo -e "${yellow}▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ${plain}"
 
-echo -e "${cyan}i am 404 😎 ${plain}"
+echo -e "${cyan}i am Mr.htun 😎 ${plain}"
